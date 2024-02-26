12f0aa588b83993036ee190b9b6a495a270720fa
Lukasz Foltyn <lukfol1@gmail.com>
2024-02-26 18:41:35 +0100

    ok

--- auth.ts
+++ auth.ts
@@ -32,7 +32,7 @@ export const { auth, signIn, signOut } = NextAuth({
             const user = await getUser(email);
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
-
+            console.log("user ok");
             if (passwordsMatch) return user;
           }
           console.log('Invalid credentials');
